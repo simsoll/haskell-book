c89b6d85759d8db073ac3734a36e6275445853df
simsoll <simon@soellingvraa.dk>
2017-10-27 22:48:59 +0200

    Last exercises for chaper 11

--- chapter11/exercises.hs
+++ chapter11/exercises.hs
@@ -245,3 +245,13 @@ coolestLtr = mostPopularLetter . concat
 
 coolestWord :: [String] -> String
 coolestWord = fst . (mostPopularCount " ")
+
+data Expr = Lit Integer | Add Expr Expr
+
+eval :: Expr -> Integer
+eval (Lit i)     = i
+eval (Add e1 e2) = eval e1 + eval e2
+
+printExpr :: Expr -> String
+printExpr (Lit i)     = show i
+printExpr (Add e1 e2) = printExpr e1 ++ " + " ++ printExpr e2
