c3407cdf01b9b3fd7a3a00d0e6e41a8013d0fb3a
simsoll <simon@soellingvraa.dk>
2017-11-02 19:31:23 +0100

    Added a solution to the last exercise in chapter 13

--- chapter13/exercises.hs
+++ chapter13/exercises.hs
@@ -18,3 +18,36 @@ isPalindrome s = reverse cleanedWord == cleanedWord
 cleanWord :: String -> String
 cleanWord s = filter (\c -> c /= ' ' && c /= '\'') $ map toLower s
 
+-- 4.
+
+type Name = String
+type Age = Integer
+data Person = Person Name Age deriving Show
+
+data PersonInvalid =
+        NameEmpty
+        | AgeTooLow
+        | PersonInvalidUnknown String
+        deriving (Eq, Show)
+
+mkPerson :: Name -> Age -> Either PersonInvalid Person
+mkPerson name age
+ | name /= "" && age > 0 = Right $ Person name age
+ | name == "" = Left NameEmpty
+ | not (age > 0) = Left AgeTooLow
+ | otherwise =
+        Left $ PersonInvalidUnknown $
+         "Name was: " ++ show name ++
+         " Age was: " ++ show age
+
+showPerson :: Either PersonInvalid Person -> String
+showPerson (Left error)   = "Error: " ++ show error
+showPerson (Right person) = "Yay! " ++ show person
+
+gimmePerson :: IO ()
+gimmePerson = do
+        putStr "Name: "
+        name <- getLine
+        putStr "Age: "
+        age <- getLine
+        putStrLn $ showPerson $ mkPerson name (read age :: Integer)
