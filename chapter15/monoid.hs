f24c9b634934a2614fabcf83ab253c0932876aec
simsoll <simon@soellingvraa.dk>
2017-11-05 09:00:12 +0100

    Added solutions to a few semigroup exercises

---
+++ chapter15/monoid.hs
@@ -0,0 +1,103 @@
+import           Control.Monad
+import           Data.Monoid
+import           Test.QuickCheck
+
+-- Optional Monoid
+
+data Optional a =
+    Nada
+    | Only a
+    deriving (Eq, Show)
+
+instance Monoid a => Monoid (Optional a) where
+    mempty = Nada
+    mappend (Only x) (Only y) = Only $ mappend x y
+    mappend (Only x) _        = Only x
+    mappend _ (Only y)        = Only y
+    mappend _ _               = Nada
+
+-- Madness
+
+type Verb = String
+type Adjective = String
+type Adverb = String
+type Noun = String
+type Exclamation = String
+
+madlibbin' :: Exclamation -> Adverb -> Noun -> Adjective -> String
+madlibbin' e adv noun adj =
+    e <> "! he said " <>
+    adv <> " as he jumped into his car " <>
+    noun <> " and drove off with his " <>
+    adj <> " wife."
+
+madlibbinBetter' :: Exclamation -> Adverb -> Noun -> Adjective -> String
+madlibbinBetter' e adv noun adj =
+    mconcat [e, "! he said ",
+        adv, " as he jumped into his car ",
+        noun, " and drove off with his ",
+        adj, " wife."]
+
+-- Testing QuickCheck's patience
+
+data Bull =
+    Fools
+    | Twoo
+    deriving (Eq, Show)
+
+instance Arbitrary Bull where
+    arbitrary = frequency [ (1, return Fools), (1, return Twoo)]
+
+instance Monoid Bull where
+    mempty = Fools
+    mappend _ _ = Fools
+
+type BullMappend = Bull -> Bull -> Bull -> Bool
+
+monoidAssoc :: (Eq m, Monoid m) => m -> m -> m -> Bool
+monoidAssoc a b c = (a <> (b <> c)) == ((a <> b) <> c)
+
+monoidLeftIdentity :: (Eq m, Monoid m) => m -> Bool
+monoidLeftIdentity a = (mempty <> a) == a
+
+monoidRightIdentity :: (Eq m, Monoid m) => m -> Bool
+monoidRightIdentity a = (a <> mempty) == a
+
+main :: IO ()
+main = do
+    let ma = monoidAssoc :: BullMappend
+        mli = monoidLeftIdentity
+        mlr = monoidRightIdentity
+    quickCheck (ma :: BullMappend)
+    quickCheck (mli :: Bull -> Bool)
+    quickCheck (mlr :: Bull -> Bool)
+
+-- Maybe Another Monoid
+
+newtype First' a =
+    First' {getFirst' :: Optional a}
+    deriving (Eq, Show)
+
+instance Monoid (First' a) where
+    mempty = First' Nada
+    mappend (First' (Only x)) _             = First' $ Only x
+    mappend (First' Nada) (First' (Only y)) = First' $ Only y
+    mappend _ _                             = First' Nada
+
+gen' :: Arbitrary a => Gen (First' a)
+gen' = do
+    a <- arbitrary
+    frequency [(1, return $ First' Nada), (3, return $ First' (Only a))]
+
+instance Arbitrary a => Arbitrary (First' a) where
+    arbitrary = gen'
+
+type FirstMappend = First' String -> First' String -> First' String -> Bool
+
+type FstId = First' String -> Bool
+
+main2 :: IO ()
+main2 = do
+    quickCheck (monoidAssoc :: FirstMappend)
+    quickCheck (monoidLeftIdentity :: FstId)
+    quickCheck (monoidRightIdentity :: FstId)
---
+++ chapter15/semigroup.hs
@@ -0,0 +1,96 @@
+import           Data.Semigroup
+import           Test.QuickCheck
+
+-- 1.
+
+data Trivial = Trivial deriving (Eq, Show)
+
+instance Semigroup Trivial where
+    _ <> _ = Trivial
+
+instance Arbitrary Trivial where
+    arbitrary = return Trivial
+
+semigroupAssoc :: (Eq m, Semigroup m) => m -> m -> m -> Bool
+semigroupAssoc a b c =
+    (a <> (b <> c)) == ((a <> b) <> c)
+
+-- 2.
+
+newtype Identity a = Identity a deriving (Eq, Show)
+
+instance Semigroup a => Semigroup (Identity a) where
+    Identity a <> Identity b = Identity $ a <> b
+
+instance Arbitrary a => Arbitrary (Identity a) where
+    arbitrary = do
+        a <- arbitrary
+        return $ Identity a
+
+-- 3.
+
+data Two a b = Two a b deriving (Eq, Show)
+
+instance (Semigroup a, Semigroup b) => Semigroup (Two a b) where
+    (Two a b) <> (Two c d) = Two (a <> c) (b <> d)
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Two a b) where
+    arbitrary = do
+        a <- arbitrary
+        b <- arbitrary
+        return $ Two a b
+
+-- 4.
+
+data Three a b c = Three a b c deriving (Eq, Show)
+
+instance (Semigroup a, Semigroup b, Semigroup c) => Semigroup (Three a b c) where
+    (Three a b c) <> (Three d e f) = Three (a <> d) (b <> e) (c <> f)
+
+instance (Arbitrary a, Arbitrary b, Arbitrary c) => Arbitrary (Three a b c) where
+    arbitrary = do
+        a <- arbitrary
+        b <- arbitrary
+        c <- arbitrary
+        return $ Three a b c
+
+
+-- 5.
+
+data Four a b c d = Four a b c d deriving (Eq, Show)
+
+instance (Semigroup a, Semigroup b, Semigroup c, Semigroup d) => Semigroup (Four a b c d) where
+    (Four a b c d) <> (Four e f g h) = Four (a <> e) (b <> f) (c <> g) (d <> h)
+
+instance (Arbitrary a, Arbitrary b, Arbitrary c, Arbitrary d) => Arbitrary (Four a b c d) where
+    arbitrary = do
+        a <- arbitrary
+        b <- arbitrary
+        c <- arbitrary
+        d <- arbitrary
+        return $ Four a b c d
+
+
+-- 6.
+
+newtype BoolConj = BoolConj Bool deriving (Eq, Show)
+
+instance Semigroup BoolConj where
+    BoolConj True <> BoolConj True = BoolConj True
+    _ <> _  = BoolConj False
+
+instance Arbitrary BoolConj where
+    arbitrary = frequency [ (1, return $ BoolConj True), (1, return $ BoolConj False)]
+
+
+type Assoc x = x -> x -> x -> Bool
+
+main :: IO()
+main = do
+    quickCheck (semigroupAssoc :: (Assoc Trivial))
+    quickCheck (semigroupAssoc :: (Assoc (Identity String)))
+    quickCheck (semigroupAssoc :: (Assoc (Two String String)))
+    quickCheck (semigroupAssoc :: (Assoc (Three String String String)))
+    quickCheck (semigroupAssoc :: (Assoc (Four String String String String)))
+    quickCheck (semigroupAssoc :: (Assoc BoolConj))
+
