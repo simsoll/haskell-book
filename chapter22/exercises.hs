cd7e0a1c1f358b80fbb28c0b4c92fb88cbabca29
simsoll <simon@soellingvraa.dk>
2017-12-21 20:17:07 +0100

    Added a few more chapter 22 solutions

--- chapter22/exercises.hs
+++ chapter22/exercises.hs
@@ -60,3 +60,45 @@ instance Applicative (Reader r) where
     (<*>) :: Reader r (a -> b) -> Reader r a -> Reader r b
     (Reader rab) <*> (Reader ra) = Reader $ \r -> rab r (ra r)
 
+-- Reader Monad
+
+-- 1.
+
+instance Monad (Reader r) where
+    return = pure
+
+    (>>=) :: Reader r a -> (a -> Reader r b) -> Reader r b
+    (Reader ra) >>= aRb = Reader $ \r -> runReader (aRb (ra r)) r
+
+-- 2.
+
+newtype HumanName =
+    HumanName String
+    deriving (Eq, Show)
+
+newtype DogName =
+    DogName String
+    deriving (Eq, Show)
+
+newtype Address =
+    Address String
+    deriving (Eq, Show)
+
+data Person =
+    Person {
+    humanName :: HumanName
+    , dogName :: DogName
+    , address :: Address
+    } deriving (Eq, Show)
+
+data Dog =
+    Dog {
+    dogsName      :: DogName
+    , dogsAddress :: Address
+    } deriving (Eq, Show)
+
+getDogRm :: Reader Person Dog
+getDogRm = do
+    name <- Reader dogName
+    addy <- Reader address
+    return $ Dog name addy
