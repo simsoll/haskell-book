14f09987e6d9227c802ece812341990644f6f64b
simsoll <simon@soellingvraa.dk>
2018-01-03 16:56:26 +0100

    Added solution to Roll Your Own exercise 2

--- chapter23/exercises.hs
+++ chapter23/exercises.hs
@@ -5,6 +5,31 @@ import           System.Random
 
 -- Roll Your Own
 
+data Die =
+      DieOne
+    | DieTwo
+    | DieThree
+    | DieFour
+    | DieFive
+    | DieSix
+    deriving (Eq, Show)
+
+intToDie :: Int -> Die
+intToDie n =
+    case n of
+        1 -> DieOne
+        2 -> DieTwo
+        3 -> DieThree
+        4 -> DieFour
+        5 -> DieFive
+        6 -> DieSix
+        -- Use 'error'
+        -- _extremely_ sparingly.
+        x ->
+            error $
+                "intToDie got non 1-6 integer: "
+                ++ show x
+
 -- 1.
 
 rollsToGetN :: Int -> StdGen -> Int
@@ -18,3 +43,17 @@ rollsToGetN n g = go 0 0 g
                 randomR (1, 6) gen
           in go (sum + die)
                 (count + 1) nextGen
+
+-- 2.
+
+rollsCountLogged :: Int -> StdGen -> (Int, [Die])
+rollsCountLogged n g = go 0 (0, []) g
+    where
+     go :: Int -> (Int, [Die]) -> StdGen -> (Int, [Die])
+     go sum (count, dies) gen
+        | sum >= n = (count, dies)
+        | otherwise =
+          let (die, nextGen) =
+                randomR (1, 6) gen
+          in go (sum + die)
+                (count + 1, (intToDie die) : dies) nextGen
