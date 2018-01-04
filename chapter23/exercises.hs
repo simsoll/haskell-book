c26a529128519a4c12a74ddb420cb892e486c9d6
simsoll <simon@soellingvraa.dk>
2018-01-04 19:57:33 +0100

    Added solutions to chapter exercises in chapter 23

--- chapter23/exercises.hs
+++ chapter23/exercises.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE InstanceSigs #-}
+
 import           Control.Applicative       (liftA3)
 import           Control.Monad             (replicateM)
 import           Control.Monad.Trans.State
@@ -57,3 +59,52 @@ rollsCountLogged n g = go 0 (0, []) g
                 randomR (1, 6) gen
           in go (sum + die)
                 (count + 1, (intToDie die) : dies) nextGen
+
+-- Write State for yourself
+
+newtype Moi s a =
+    Moi { runMoi :: s -> (a, s)}
+
+instance Functor (Moi s) where
+    fmap :: (a -> b) -> Moi s a -> Moi s b
+    fmap f (Moi g) = Moi $ (\(a, s) -> (f a, s)) . g
+
+instance Applicative (Moi s) where
+    pure :: a -> Moi s a
+    pure a = Moi $ (\s -> (a, s))
+
+    (<*>) :: Moi s (a -> b) -> Moi s a -> Moi s b
+    (Moi f) <*> (Moi g) = Moi $ (\(ab, s) -> (ab (fst $ g s), s)) . f
+
+instance Monad (Moi s) where
+    return = pure
+
+    (>>=) :: Moi s a -> (a -> Moi s b) -> Moi s b
+    (Moi f) >>= g = Moi $ (\(a, s) -> runMoi (g a) s) . f
+
+-- Chapter exercises
+
+-- 1.
+
+get' :: Moi s s
+get' = Moi $ (\s -> (s, s))
+
+-- 2.
+
+put' :: s -> Moi s ()
+put' s = Moi $ (\_ -> ((), s))
+
+-- 3.
+
+exec' :: Moi s a -> s -> s
+exec' (Moi sa) s = snd $ sa s
+
+-- 4.
+
+eval' :: Moi s a -> s -> a
+eval' (Moi sa) s = fst $ sa s
+
+-- 5.
+
+modify'' :: (s -> s) -> Moi s ()
+modify'' f = Moi $ (\s -> ((), f s))
