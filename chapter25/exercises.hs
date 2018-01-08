206d7890bb3ca1343247abddbccc902cc75ceada
simsoll <simon@soellingvraa.dk>
2018-01-08 21:04:47 +0100

    Added a few more solutions to chapter 25

--- chapter25/exercises.hs
+++ chapter25/exercises.hs
@@ -7,7 +7,7 @@ import           Control.Applicative
 
 newtype Compose f g a =
     Compose { getCompose :: f (g a) }
-    deriving (Eq, Show)
+    deriving (Show)
 
 instance (Functor f, Functor g) => Functor (Compose f g) where
     fmap f (Compose fga) = Compose $ (fmap . fmap) f fga
@@ -30,3 +30,68 @@ instance (Foldable f, Foldable g) => Foldable (Compose f g) where
 
 instance (Traversable f, Traversable g) => Traversable (Compose f g) where
     traverse f (Compose fga) = Compose <$> (traverse . traverse) f fga
+
+-- Bifunctor
+
+class Bifunctor p where
+    {-# MINIMAL bimap | first, second #-}
+    bimap :: (a -> b) -> (c -> d) -> p a c -> p b d
+    bimap f g = first f . second g
+
+    first :: (a -> b) -> p a c -> p b c
+    first f = bimap f id
+
+    second :: (b -> c) -> p a b -> p a c
+    second = bimap id
+
+-- 1.
+
+data Deux a b = Deux a b
+    deriving (Show)
+
+instance Bifunctor Deux where
+    bimap f g (Deux a b) = Deux (f a) (g b)
+
+-- 2.
+
+data Const' a b = Const' a
+    deriving (Show)
+
+instance Bifunctor Const' where
+    bimap f _ (Const' a) = Const' (f a)
+
+-- 3.
+
+data Drei a b c = Drei a b c
+    deriving (Show)
+
+instance Bifunctor (Drei a) where
+    bimap f g (Drei a b c) = Drei a (f b) (g c)
+
+-- 4.
+
+data SuperDrei a b c = SuperDrei a b
+    deriving (Show)
+
+instance Bifunctor (SuperDrei a) where
+    bimap f _ (SuperDrei a b) = SuperDrei a (f b)
+
+-- 5.
+
+data SemiDrei a b c = SemiDrei a
+
+instance Bifunctor (SemiDrei a) where
+    bimap _ _ (SemiDrei a) = SemiDrei a
+
+-- 6.
+
+data Quadriceps a b c d = Quadzzz a b c d
+
+instance Bifunctor (Quadriceps a b) where
+    bimap f g (Quadzzz a b c d) = Quadzzz a b (f c) (g d)
+
+-- 7.
+
+instance Bifunctor Either where
+    bimap f _ (Left a)  = Left (f a)
+    bimap _ g (Right b) = Right (g b)
